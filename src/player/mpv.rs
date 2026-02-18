eb60408478aa5c9ef07dfdff8de0755423d514c7
exilonium <exilonium@proton.me>
2026-02-18 22:52:19 +0530

    added a playing state to the song

--- src/player/mpv.rs
+++ src/player/mpv.rs
@@ -9,11 +9,15 @@ const SOCKET: &str = "/tmp/music-tui-mpv";
 
 pub struct Player {
     pub process: Option<Child>,
+    pub playing: bool,
 }
 
 impl Player {
     pub fn new() -> Self {
-        Self { process: None }
+        Self {
+            process: None,
+            playing: false,
+        }
     }
     pub fn play(&mut self, url: &str) -> Result<()> {
         if let Some(mut child) = self.process.take() {
@@ -28,20 +32,23 @@ impl Player {
             .stderr(Stdio::null())
             .spawn()?;
         self.process = Some(child);
+        self.playing = true;
         Ok(())
     }
     pub fn stop(&mut self) {
         if let Some(mut child) = self.process.take() {
             let _ = child.kill();
+            self.playing = false;
         }
     }
-    pub fn toggle_pause(&self) -> Result<()> {
+    pub fn toggle_pause(&mut self) -> Result<()> {
         let mut socket = UnixStream::connect(SOCKET)?;
 
         let cmd = r#"{ "command": ["cycle", "pause"] }"#; // this is mpv json IPC Command
 
         socket.write_all(cmd.as_bytes())?;
         socket.write_all(b"\n")?;
+        self.playing = !self.playing;
         Ok(())
     }
 }
